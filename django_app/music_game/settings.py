029420bf03e327b467369159ad2e837ecc0dee8e
fynn2301 <50694101+fynn2301@users.noreply.github.com>
2024-12-07 19:12:48 +0100

    added new static path

--- django_app/music_game/settings.py
+++ django_app/music_game/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/game/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Add this to tell Django where to find additional static files
