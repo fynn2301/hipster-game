d157f4f0f6375b424ea0100b7faa9e29abe445e0
fynn2301 <50694101+fynn2301@users.noreply.github.com>
2024-12-08 22:09:44 +0100

    fix csrf

--- django_app/music_game/settings.py
+++ django_app/music_game/settings.py
@@ -118,11 +118,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-CSRF_TRUSTED_ORIGINS = ['https://fynn2301.pythonanywhere.com']
+CSRF_TRUSTED_ORIGINS = ['http://192.168.0.77']
 CSRF_COOKIE_SAMESITE = 'Lax'
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
